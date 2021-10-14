1c8e341eb953b1adfaa2e2aafef67b6d19c0c143
@BennEntterprise <kyle.bennett@betweenfiveandnine.com>
2021-10-14 09:19:34 -0400

    feat: add basic text analysis
    Includes:
     - paragraph count
     - sentance count
     - word count
     - character count
     - average word size

--- analysis.ts
+++ analysis.ts
@@ -1,13 +1,44 @@
-console.log('Hello');
-
 import fs from 'fs'
 
-let article;
+// Interfaces
+interface ISummary{
+    paragraphCount?: number;
+    sentanceCount?: number;
+    wordCount?: number;
+    characterCount?: number;
+    averageWordSize?: string;
+}
+
+const summary:ISummary = {
+    paragraphCount: 0,
+    sentanceCount: 0,
+    wordCount: 0,
+    characterCount: 0,
+    averageWordSize: ''
+}
+
+// Read in the Article
+let article = '';
 try {
     article = fs.readFileSync('./Day1.txt', 'utf8')
 } catch (err){
     console.error(err)
 }
 
-console.log(article);
-    
+// Basic Analysis
+const paragraphs = article?.split('\n').filter(Boolean);
+const sentances = article?.split('.').filter(Boolean);
+const words = article?.split(/\s/).filter(Boolean);
+const totalCharInWords = words.map(w => w.length).reduce( (acc, current ) => acc + current,  0 )
+const avgWordSize = (totalCharInWords / words.length).toFixed(2);
+
+// Compile the Summary
+summary.paragraphCount = paragraphs.length;
+summary.sentanceCount = sentances.length;
+summary.wordCount = words.length;
+summary.characterCount = article.length;
+summary.averageWordSize = avgWordSize;
+
+// Output the summary
+// console.log(article);
+console.log(summary);
