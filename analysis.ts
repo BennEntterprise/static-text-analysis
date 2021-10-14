1fafc0c3e6ce2db94ce17d40bdd3322e1f4ea403
@BennEntterprise <kyle.bennett@betweenfiveandnine.com>
2021-10-14 09:33:03 -0400

    feat: add average words per sentance metric

--- analysis.ts
+++ analysis.ts
@@ -5,6 +5,7 @@ interface ISummary{
     paragraphCount?: number;
     sentanceCount?: number;
     wordCount?: number;
+    averageWordsInSentance: string
     characterCount?: number;
     averageWordSize?: string;
 }
@@ -13,6 +14,7 @@ const summary:ISummary = {
     paragraphCount: 0,
     sentanceCount: 0,
     wordCount: 0,
+    averageWordsInSentance: '',
     characterCount: 0,
     averageWordSize: ''
 }
@@ -27,9 +29,10 @@ try {
 
 // Basic Analysis
 const paragraphs = article?.split('\n').filter(Boolean);
-const sentances = article?.split('.').filter(Boolean);
+const sentances = article?.split(/\.|\?|\!/).filter(Boolean);
 const words = article?.split(/\s/).filter(Boolean);
-const totalCharInWords = words.map(w => w.length).reduce( (acc, current ) => acc + current,  0 )
+const averageWordsInSentance = words.length / sentances.length;
+const totalCharInWords = words.map(w => w.length).reduce( (acc, current ) => acc + current,  0 );
 const avgWordSize = (totalCharInWords / words.length).toFixed(2);
 
 // Compile the Summary
@@ -38,6 +41,7 @@ summary.sentanceCount = sentances.length;
 summary.wordCount = words.length;
 summary.characterCount = article.length;
 summary.averageWordSize = avgWordSize;
+summary.averageWordsInSentance = averageWordsInSentance.toFixed(2);
 
 // Output the summary
 // console.log(article);
